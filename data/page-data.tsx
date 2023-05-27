84923fe3ebba2743cad93e74d21872d200db5a63
Bine Kosmac <bine.valo@gmail.com>
2023-05-27 08:58:53 +0200

    Modified personal data

--- data/page-data.tsx
+++ data/page-data.tsx
@@ -51,23 +51,10 @@ export const professionalData = {
 	title: "Professional Experience",
 	experiences: [
 		{
-			role: "Data Scientist, Acme Inc. (2021-Present)",
+			role: "Emergency Room Registered Nurse, UKC Maribor (2020 - present)",
 			description:
-				"Design and implement machine learning models to analyze large datasets and drive business decisions. Collaborate with cross-functional teams to develop and launch new products and features. Provide technical guidance and mentorship to junior data scientists. Conduct regular presentations to stakeholders on the findings and insights generated from data analysis.",
+				"Respond to medical emergencies, work in tandem with coworkers and doctors to provide care for patients, assess patient needs, administer intravenous/oral/intramuscular/subcutaneous therapy, start IV lines, draw blood, disinfect work area and equipment, clean wounds, test medical equipment, fill out paperwork.",
 			current: true,
 		},
-
-		{
-			role: "Machine Learning Engineer, XYZ Corp. (2019-2021)",
-			description:
-				"Designed and implemented advanced machine learning algorithms to improve predictive accuracy and performance. Built and maintained scalable machine learning infrastructure using cloud computing platforms. Conducted data exploratory analysis to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
-			current: false,
-		},
-		{
-			role: "Machine Learning Engineer, ABX Corp. (2020-2021)",
-			description:
-				"Designed and implemented advanced machine learning algorithms to improve predictive accuracy and performance. Built and maintained scalable machine learning infrastructure using cloud computing platforms. Conducted data exploratory analysis to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
-			current: false,
-		},
-	],
+	]
 };
